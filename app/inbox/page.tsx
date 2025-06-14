f849cf1a1b4377fd6501eed5ea1f3143d9c31215
Comdev2k23 <comdev084@gmail.com>
2025-06-14 23:16:21 +0800

    added inbox features

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,7 +1,90 @@
-import React from 'react'
+'use client'
+
+type Schedule = {
+  _id: string
+  studentName: string
+  manuscriptTitle: string
+  defenseDate: string
+  panel: string
+  panelStatus: {
+    name: string
+    status: string
+  }[]
+}
+
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'sonner'
+import axios from 'axios'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import { AlertCircleIcon, BadgeCheckIcon, CheckIcon } from "lucide-react"
 
 export default function InboxPage() {
+  const [schedules, setSchedules] = useState<Schedule []>([])
+
+
+  useEffect(()=> {
+
+      async function fetchData () {
+        
+        try {
+          
+          const token = localStorage.getItem('token')
+          if(!token){
+            return toast('Error: Invalid token or expired, please login again')
+          }
+
+          const res = await axios.get(
+            "https://mss-express.onrender.com/api/schedules/admin/ready-schedules", {
+                        headers :{
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+          )
+          setSchedules(res.data.schedules)
+            console.log(res.data)
+
+        } catch (error) {
+            toast.error(`${error}`)          
+        }
+      }
+    fetchData()
+  },[])
+
   return (
-    <div>InboxPage</div>
+      <div className='mt-4'>
+          <h1 className="text-2xl font-bold">
+      Schedules waiting for admin approval: 
+    </h1>
+       <div className="flex max-h-72 gap-4 p-4 mt-4">
+      { schedules.length === 0 && <p>No schedules ready for approval.</p>}
+      
+      {schedules.map((schedule) => (
+        <Card key={schedule._id}>
+          <CardHeader>
+            <CardTitle>{schedule.manuscriptTitle}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Student:{schedule.studentName}</p>
+            <p><strong>Date:</strong> {new Date(schedule.defenseDate).toLocaleDateString()}</p>
+            <p><strong>Panel:</strong></p>
+            <ul className="ml-4 list-disc ">
+              {schedule.panelStatus.map((panel, idx) => (
+                <li key={idx}>
+                  {panel.name} 
+                   <Badge variant="secondary" className="bg-green-500 text-white ml-4"  >
+                    <BadgeCheckIcon />
+                    {panel.status}
+                  </Badge>  
+                </li>
+              ))}
+            </ul>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+      </div>
+   
   )
 }
--- components/inbox-notification.tsx
+++ components/inbox-notification.tsx
@@ -4,26 +4,9 @@ import React from 'react'
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { Badge } from "@/components/ui/badge"
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { AlertCircleIcon, BadgeCheckIcon, CheckIcon } from "lucide-react"
 
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
 import { toast } from 'sonner'
 
-type PanelStatus = {
-  name: string
-  status: string
-}   
 
 export default function InboxNotification() {
     const [notifCount, setNotifCount] = useState(0)
